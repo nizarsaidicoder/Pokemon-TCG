a5967714d0bf3f4a7de11329a4e624935f272904
nizar <nizar.saidi.coder@gmail.com>
2024-04-23 16:49:57 +0200

    Started The Field Class implementation

--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Field {
+//  - pokemons: Pokemon[]
+//  + estVide() : Boolean
+//  + <<Create>> Terrain()
+//  + placerPokemon(pokemon: Pokemon)
+//  + attaquer(adversaire: Terrain)
+//  + afficheDebutJeu()
+//  + getPokemons() : Pokemon[]
+//  + afficherTerrain()
+    private ArrayList<Pokemon> pokemons;
+    public Field() {
+        pokemons = new ArrayList<Pokemon>();
+    }
+    public boolean isEmpty()
+    {
+        return pokemons.isEmpty();
+    }
+    public void addPokemon(Pokemon pokemon)
+    {
+        pokemons.add(pokemon);
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,4 @@
-public class Game {
+public class Game
+{
+
 }
