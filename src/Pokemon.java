0c35d8e35daf13d1cbc6a5b0802df6a47fd8f5e5
nizar <nizar.saidi.coder@gmail.com>
2024-04-23 14:37:02 +0200

    Added Empty classes

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,7 +1,2 @@
-public class Pokemon
-{
-  public static void main(String args[])
-  {
-    System.out.println("Lancement du jeu ...");
-  }
+public class Pokemon {
 }
---
+++ src/Pookemon.java
@@ -0,0 +1,7 @@
+public class Pookemon
+{
+  public static void main(String args[])
+  {
+    System.out.println("Lancement du jeu ...");
+  }
+}
