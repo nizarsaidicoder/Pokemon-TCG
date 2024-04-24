02af9b54d49db841e123194e082c71b02b96ea84
nizar <nizar.saidi.coder@gmail.com>
2024-04-24 12:31:46 +0200

    Started the display game status function

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -5,16 +5,28 @@ import Others.Graveyard;
 import Others.Hand;
 import Pokemon.Pokemon;
 
+import java.util.ArrayList;
+
 public class Player {
-    private Field m_field;
-    private Graveyard m_graveyard;
-    private Deck m_deck;
-    private Hand m_hand;
-    private int m_playerNumber;
+    protected Field m_field;
+    protected Graveyard m_graveyard;
+    protected Deck m_deck;
+    protected Hand m_hand;
+    protected int m_playerNumber;
     
     public Player()
     {
         // Initialise le deck, la main, le cimetière et le terrain
+        m_field = new Field();
+        m_field.addPokemon(new Pokemon("Pikachu", 100, 50,50, "earth"));
+        m_field.addPokemon(new Pokemon("Bulbizarre", 100, 50, 80,"fire"));
+        m_field.addPokemon(new Pokemon("Salamèche", 100, 50, 100,"water"));
+        m_graveyard = new Graveyard();
+        ArrayList<Pokemon> pokemons = new ArrayList<>();
+        pokemons.add(new Pokemon("Pikachu", 100, 50,50, "earth"));
+        m_deck = new Deck(pokemons);
+        pokemons.add(new Pokemon("Raikachu", 100, 50,50, "earth"));
+        m_hand = new Hand(pokemons);
     }
     public void draw()
     {
@@ -27,22 +39,35 @@ public class Player {
     {
         //
     }
+    public void displayPlayer()
+    {
+        // Affiche le joueur
+        System.out.println("Player : " + m_playerNumber);
+        showField();
+        showDeck();
+        showGraveyard();
+        showHand();
+    }
 
     public void showDeck()
     {
         // Affiche le deck
+        m_deck.displayDeck();
     }
     public void showGraveyard()
     {
         // Affiche le cimetière
+        m_graveyard.displayGraveyard();
     }
     public void showField()
     {
         // Affiche le terrain
+        m_field.displayField();
     }
     public void showHand()
     {
         // Affiche la main
+        m_hand.displayHand();
     }
 
 
--- src/Pookemon.java
+++ src/Pookemon.java
@@ -6,27 +6,7 @@ public class Pookemon {
   public static void main(String args[])
   {
     System.out.println("Lancement du jeu ...");
-    Field field = new Field();
-    field.addPokemon(new Pokemon("Pikachu", 100, 50,50, "earth"));
-    field.addPokemon(new Pokemon("Bulbizarre", 100, 50, 80,"fire"));
-    field.addPokemon(new Pokemon("Salamèche", 100, 50, 100,"water"));
-    field.addPokemon(new Pokemon("Carapuce", 100, 50, 120,"air"));
-    field.displayField();
-
-    Hand hand = new Hand();
-    hand.addPokemon(new Pokemon("Pikachu", 100, 50,50, "earth"));
-    hand.addPokemon(new Pokemon("Bulbizarre", 100, 50, 80,"fire"));
-    hand.displayHand();
-
-    ArrayList<Pokemon> pokemons = new ArrayList<>();
-    pokemons.add(new Pokemon("Pikachu", 100, 50,50, "earth"));
-    pokemons.add(new Pokemon("Bulbizarre", 100, 50, 80,"fire"));
-    pokemons.add(new Pokemon("Salamèche", 100, 50, 100,"water"));
-
-    Deck deck = new Deck(pokemons);
-    deck.displayDeck();
-
-    Graveyard graveyard = new Graveyard();
-    graveyard.displayGraveyard();
+    Game game = new Game();
+    game.showGameStatus();
   }
 }
