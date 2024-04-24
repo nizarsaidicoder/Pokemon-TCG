23a53d5fb1904aba21863a01a5c2c47ff16a7b71
Nesrine HAGGUI <haggui@unistra.fr>
2024-04-24 17:16:11 +0200

    Tests attaque entre pokemon (en commentaires dans la classe 'Pookemon')

--- src/Pookemon.java
+++ src/Pookemon.java
@@ -8,5 +8,13 @@ public class Pookemon {
     System.out.println("Lancement du jeu ...");
     Game game = new Game();
     game.showGameStatus();
+
+    //tests Pokemon
+    // Pokemon pikachu = new Pokemon("Pikachu", 100, 100,50, "earth");
+    // Pokemon bulbizarre = new Pokemon("Bulbizarre", 100, 100, 80,"fire");
+
+    // System.out.println(bulbizarre.toString());
+    // pikachu.attack(bulbizarre);
+    // System.out.println(bulbizarre.toString());
   }
 }
