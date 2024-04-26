cfe361c98fd39f3959df21f38d0239a9662cb1bb
nizar <nizar.saidi.coder@gmail.com>
2024-04-26 16:18:38 +0200

    Modified the backbone of Pokemon

--- src/Pokemon/Pokemon.java
+++ src/Pokemon/Pokemon.java
@@ -7,6 +7,7 @@ public class Pokemon
   private int m_hpMax;
   private int m_attack;
   private Affinity m_affinity;
+  private boolean m_isPlayable;
 
   //constructeur
   public Pokemon(String name, int hp, int hpMax, int attack, Affinity affinity)
@@ -30,12 +31,19 @@ public class Pokemon
     }
   }
   /*
-    * Méthode pour vérifier si le pokemon est vivant
+   * Méthode pour vérifier si le pokemon est vivant
    */
   public boolean isAlive()
   {
     // Le pokemon est vivant si et seulement si m_hp > 0
     return false;
+  }
+  /*
+   * Méthode pour vérifier si le pokemon est jouable
+   */
+  public boolean isPlayable()
+  {
+    return m_isPlayable;
   }
 
   //accesseurs
@@ -45,21 +53,24 @@ public class Pokemon
   }
   public int getHP()
   {
-      return m_hp;
+    return m_hp;
   }
   public int getHPMax()
   {
-      return m_hpMax;
+    return m_hpMax;
   }
   public int getAttack()
   {
-      return m_attack;
+    return m_attack;
   }
   public Affinity getAffinity()
   {
-      return m_affinity;
+    return m_affinity;
   }
-
+  public void setPlayable(boolean isPlayable)
+  {
+    m_isPlayable = isPlayable;
+  }
   @Override
   public String toString()
   {
